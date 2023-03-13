a8581d6237ed0d73d9d879d5ec406954e3cc14e0
lisalindqvist95 <lindqvist.lisa@hotmail.com>
2023-03-13 19:43:28 +0000

    .

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//L: standard function below:
+//function FunctionalGreeting() {
+//    return <h1>Hello from React!</h1>
+//}:
+
+//L: arrow function below:
+const FunctionalGreeting = () => <h1>Hello from React!</h1>;
+export default FunctionalGreeting;
